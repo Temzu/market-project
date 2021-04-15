0ef56d93ccba2192d756fad7d9cc3083b140e4e5
Artem <40913z@mail.ru>
2021-04-15 19:40:13 +0300

    sptiting into modules

---
+++ ms-product/src/main/java/com/temzu/market_project/msproduct/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.temzu.market_project.msproduct.repositories;
+
+import com.temzu.market_project.msproduct.model.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
