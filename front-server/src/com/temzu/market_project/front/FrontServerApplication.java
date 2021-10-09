7bf09da2147da9b91e149d614fcb51f2606182b3
Temzu <40913z@mail.ru>
2021-10-09 19:47:19 +0300

    front

---
+++ front-server/src/com/temzu/market_project/front/FrontServerApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.market2.frontserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontServerApplication.class, args);
+    }
+
+}
